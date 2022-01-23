1e79e0a20b85a7bd06d361bbe749aaccf6180e9e
jayeen28 <jayeen6783@gmail.com>
2022-01-24 00:17:36 +0600

    bot working

---
+++ index.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+const handleSendMessage = async (page) => {
+
+}
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false, executablePath: 'google-chrome-stable', args: [
+            '--start-maximized',
+            '--disable-notifications'
+        ]
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 9768 })
+    //go to facebook
+    await page.goto('https://facebook.com');
+    //write email
+    await page.type('#email', `${process.env.FACEBOOK_EMAIL}`);
+    //write pass
+    await page.type('#pass', `${process.env.FACEBOOK_PASS}`);
+    //hit login
+    await page.evaluate(() => {
+        const loginButton = document.getElementsByName('login')[0];
+        loginButton.click()
+    });
+    //wait until loaded
+    await page.waitForNavigation();
+    //go to message room
+    //go to mobile version
+    await page.goto(`${process.env.FACEBOOK_TARGETED_INBOX}`);
+    await page.evaluate(() => {
+        const input = document.getElementById('composerInput');
+        const send = document.getElementsByName('send')[0];
+        // input.focus();
+        setInterval(async () => {
+            input.focus();
+            input.innerText = "Hello, I am Jayeen's bot";
+            send.click();
+        }, 4000);
+    })
+})();
